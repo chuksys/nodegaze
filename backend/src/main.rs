2c857afbb28737c0f1680fd7542b36999359ac18
Chuks Agbakuru <chuksagbakuru@gmail.com>
2025-07-03 17:17:05 +0100

    fixup! Implement Node Authentication

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -9,14 +9,14 @@ mod errors;
 mod services;
 mod api;
 
-use axum::{routing::{get, post}, Router};
+use axum::{routing::get, Router};
 use std::net::SocketAddr;
 
 #[tokio::main]
 async fn main() {
     let app = Router::new()
         .route("/", get(root_handler))
-        .route("/api/node", post(api::node::routes::node_router));
+        .route("/api/node", api::node::routes::node_router);
     
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
